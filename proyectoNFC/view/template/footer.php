ebf5b5d700893707015542a5c7f2ccd6a179dcd6
a22masoja <113766895+a22masoja@users.noreply.github.com>
2022-12-08 11:43:20 +0100

    Delete proyectoNFC directory

--- proyectoNFC/view/template/footer.php
+++
@@ -1,6 +0,0 @@
-        </div>
-        <footer class="py-3 fixed-bottom">
-            <p class="text-center text-muted">Copyright &copy; Manu Aranda | Fran Sánchez 2022 y Javier Mariscal | Rafa | Miguel 2022</p>
-        </footer>
-    </body>
-</html>
