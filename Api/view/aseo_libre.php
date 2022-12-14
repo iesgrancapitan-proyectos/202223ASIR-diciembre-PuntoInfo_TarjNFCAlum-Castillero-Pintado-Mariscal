cacf5cd5327322e007d3bfeb90f0ddcf6079ff6c
a22masoja <113766895+a22masoja@users.noreply.github.com>
2022-12-14 21:44:28 +0100

    Delete Api directory

--- Api/view/aseo_libre.php
+++
@@ -1,9 +0,0 @@
-<div class="text-center w-100">
-    <p>Aseo libre</p>
-</div>
-
-<script>
-    window.onload = function () {
-        document.getElementsByTagName('body')[0].className = 'verde';
-    };
-</script>
--- Api/view/aseo_ocupado.php
+++
@@ -1,9 +0,0 @@
-<div class="text-center w-100">
-    <p>Aseo ocupado, intententelo mas tarde</p>
-</div>
-
-<script>
-    window.onload = function () {
-        document.getElementsByTagName('body')[0].className = 'rojo';
-    };
-</script>
