908cd77b7bd65332b6d5fb84bef8750544d7d4c1
miguel2343 <77286381+miguel2343@users.noreply.github.com>
2022-12-18 23:20:42 +0100

    Add files via upload

---
+++ Api/model/conexionDB.php
@@ -0,0 +1,20 @@
+<?php
+//para conectar una BBDD
+class conexiondb
+{
+    public function initConex()
+    {
+        $servername = "localhost";
+        $username = "user";
+        $password = "user";
+        $bd = "intranet";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $bd);
+        $conn->set_charset("utf8");
+        // Check connection
+        if ($conn->connect_error) {
+            die("Conexión falla " . $conn->connect_error);
+        }
+        return $conn;
+    }
+}
